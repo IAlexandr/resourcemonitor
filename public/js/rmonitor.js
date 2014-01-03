f4c3f588c24fa248efba99a2503a735f86b1cd8c
Aleksandr Ivanov <aivanov@informatica.ru>
2014-01-03 21:17:56 +0400

    добавление контроллера ServicesCtrl, вывод списка сервисов

--- public/js/rmonitor.js
+++ public/js/rmonitor.js
@@ -1,3 +1,17 @@
 /**
  * Created by aivanov on 03.01.14.
  */
+function ServicesCtrl($scope, $http){
+
+    $scope.services = null;
+    // получение списка сервисов
+    $scope.getServices = function(){
+        $http.get('/services.json').success(function (res){
+            $scope.services = res;
+        });
+    }
+
+    $scope.refresh = function (){
+       //механизм проверки связи с сервисами.
+    }
+}
