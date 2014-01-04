ae95376fe09e30feb2cc533396b8d05223cc4117
Aleksandr Ivanov <aivanov@informatica.ru>
2014-01-04 14:22:39 +0400

    добавление адреса на который будут отправляться запросы в целях проверки связи с сервисом. ( на сервере)

--- app.js
+++ app.js
@@ -4,11 +4,19 @@
 var port = 3000;
 
 var express = require('express');
+var urltest = require('./lib/urltest');
 
 var app = express();
 // важен порядок
 app.use(express.logger('dev')); // Вывод отладочных сообщений в консоль
 app.use(express.static(__dirname + '/public'));
+app.use('/testurl', function(req, res){
+    console.log(req.query);
+    var t = req.query;
+    urltest.testUrl(req.query.url, function(err, result){
+        res.send(result);
+    });
+});
 app.use('/',function(req, res){
     res.send(404,'Not found');
 });
