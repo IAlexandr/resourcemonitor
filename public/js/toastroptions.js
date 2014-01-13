0246a268d04ddc4992436a8e4621f131e62bac3f
Aleksandr Ivanov <aivanov@informatica.ru>
2014-01-13 14:06:00 +0400

    доб. модуля toastroptions и ссылки на него

---
+++ public/js/toastroptions.js
@@ -0,0 +1,17 @@
+/**
+ * Created by aivanov on 13.01.14.
+ */
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "positionClass": "toast-top-right",
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "1000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
