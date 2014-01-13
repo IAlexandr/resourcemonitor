c0e841b96394eeda10b31b72a8377b6ab6bb05b0
Aleksandr Ivanov <aivanov@informatica.ru>
2014-01-13 14:54:58 +0400

    изм. выключил отображение кнопки закрыть в тостер

--- public/js/toastroptions.js
+++ public/js/toastroptions.js
@@ -2,13 +2,13 @@
  * Created by aivanov on 13.01.14.
  */
 toastr.options = {
-    "closeButton": true,
+    "closeButton": false,
     "debug": false,
     "positionClass": "toast-top-right",
     "onclick": null,
     "showDuration": "300",
     "hideDuration": "1000",
-    "timeOut": "1000",
+    "timeOut": "1500",
     "extendedTimeOut": "1000",
     "showEasing": "swing",
     "hideEasing": "linear",
