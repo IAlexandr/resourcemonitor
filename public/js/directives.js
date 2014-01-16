354865df716e7fe668921c5b086ec3a57f948f72
Aleksandr Ivanov <aivanov@informatica.ru>
2014-01-16 17:10:37 +0400

    доб. сохранение координат
    добавил сервис sService в директиву dragBox

--- public/js/directives.js
+++ public/js/directives.js
@@ -2,14 +2,36 @@
  * Created by aivanov on 15.01.14.
  */
 
-serviceModule.directive('dragBox', function () {
+serviceModule.directive('dragBox', ['sService', function (sService) {
     return {
         templateUrl: "htmltemplates/dragBox.html",
         scope: {
             serv: '='
         },
         link: function (scope, element, attrs) {;
-            element.draggable({ containment: "#containment-wrapper", cursor: "move", scroll: false });
+            element.draggable({
+                stop: function(evt) {
+                    scope.serv.pX = evt.pageX;
+                    scope.serv.pY = evt.pageY;
+                    var newarr = [];
+                    sService.get(function (res) {
+                        _.each(res, function (val) {
+                            if (scope.serv.name == val.name){
+                                val = scope.serv;
+                            }
+                            newarr.push(_.pick(val, 'name', 'FeatureServiceUrl', 'id', 'pX', 'pY'));
+                        });
+                        sService.post(newarr, function (res){
+                            var t = res;
+                        });
+                        return res;
+                    });
+                },
+                containment: "#containment-wrapper",
+                cursor: "move",
+                scroll: false,
+                opacity: 0.80
+            });
         }
     }
-});
+}]);
