28ef469f96d0e3205289844ed4c7434aadd7a5b1
Aleksandr Ivanov <aivanov@informatica.ru>
2014-01-21 09:32:25 +0400

    форматирование текста, удалил  отступы

--- public/js/services.js
+++ public/js/services.js
@@ -4,29 +4,29 @@
 var serviceModule = angular.module('ServiceModule', ['ngRoute'])
 serviceModule.config(function ($routeProvider) {
     $routeProvider
-            .when('/table', {templateUrl: 'partials/table.html', controller: 'ServicesCtrl'})
-            .when('/schema', {templateUrl: 'partials/schema.html', controller: 'ServicesCtrl'})
-            .otherwise({redirectTo: '/table'});
-    });
+        .when('/table', {templateUrl: 'partials/table.html', controller: 'ServicesCtrl'})
+        .when('/schema', {templateUrl: 'partials/schema.html', controller: 'ServicesCtrl'})
+        .otherwise({redirectTo: '/table'});
+});
 serviceModule.factory('sService', ['$http', function ($http) {
-        var services = {};
-        services.get = function (callback) {
-            return $http.get('services').success(function (res) {
+    var services = {};
+    services.get = function (callback) {
+        return $http.get('services').success(function (res) {
+            callback(res);
+            return res;
+        });
+    };
+    services.checkConnection = function (url, callback) {
+        $http.get('testurl?url=' + url, {cache: false}).success(function (res) {
+            callback(res);
+        });
+    };
+    services.post = function (newarrayservices, callback) {
+        $http.post('services', newarrayservices)
+            .success(function (res) {
                 callback(res);
-                return res;
             });
-        };
-        services.checkConnection = function (url, callback) {
-            $http.get('testurl?url=' + url, {cache: false}).success(function (res) {
-                callback(res);
-            });
-        };
-        services.post = function (newarrayservices, callback) {
-            $http.post('services', newarrayservices)
-                .success(function (res) {
-                    callback(res);
-                });
-        };
-        return services;
-    }]);
+    };
+    return services;
+}]);
 
