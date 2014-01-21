40e159bb0c7eb6f3fae5dd6aade230e541e3a3f9
Aleksandr Ivanov <aivanov@informatica.ru>
2014-01-21 09:26:59 +0400

    форматирование текста, доб. отступы

--- public/js/directives.js
+++ public/js/directives.js
@@ -10,19 +10,19 @@ serviceModule.directive('dragBox', ['sService', function (sService) {
         },
         link: function (scope, element, attrs) {
             element.draggable({
-                stop: function(evt, ui) {
+                stop: function (evt, ui) {
                     var se = scope.serv;
                     se.pX = ui.position.left;
                     se.pY = ui.position.top;
                     var newarr = [];
                     sService.get(function (res) {
                         _.each(res, function (val) {
-                            if (se.name == val.name){
+                            if (se.name == val.name) {
                                 val = se;
                             }
                             newarr.push(_.pick(val, 'name', 'FeatureServiceUrl', 'id', 'pX', 'pY'));
                         });
-                        sService.post(newarr, function (res){
+                        sService.post(newarr, function (res) {
                             var t = res;
                         });
                         return res;
